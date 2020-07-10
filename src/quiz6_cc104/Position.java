0d5d8cf11583222cf5ed96c65344915ee7f7b0ea
Fairy <Fairy@LAPTOP-JIN4FP2O.local>
2020-07-10 09:55:42 +0800

    Quiz6_july9

---
+++ src/quiz6_cc104/Position.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz6_cc104;
+
+/**
+ *
+ * @author Fairy
+ */
+public interface Position<E>{
+    public E getElement() throws IllegalStateException;
+}
